06a7577edead6db783d41132c9f7ca15b1421da3
IronFist <firon1285@gmail.com>
2026-04-03 13:08:58 +0530

    Add GitIgnore

--- db/index.js
+++ db/index.js
@@ -1,10 +1,14 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const { Pool } = pkg;
 
 export const pool = new Pool({
-    user: "postgres",
-    database: "Project-Management-System-DB",
-    password: "mike5000",
-    host: "localhost",
-    port: 5432
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
 });
